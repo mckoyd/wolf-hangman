98b0a2b88c393d4cb5d63045d527ab677c1de3cf
mckoyd <dominick.mckoy@gmail.com>
2024-02-20 15:18:55 -0800

    adds menu page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React from "react";
+
+import { ReactComponent as GameTitle } from "./assets/images/game-title.svg";
+import { ReactComponent as IconPlay } from "./assets/images/icon-play.svg";
 import "./App.css";
 
 const App: React.FC = () => {
-  return <div className="App">Hangman</div>;
+  return (
+    <div className="main">
+      <div className="main-card">
+        <GameTitle className="game-title" />
+        <div className="icon-container">
+          <IconPlay className="icon-play" />
+        </div>
+        <button type="button" className="button how-to-play">
+          How To Play
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default App;

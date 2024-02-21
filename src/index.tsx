9357a3af736c9a56751522f4b00f0c75618a7f54
mckoyd <dominick.mckoy@gmail.com>
2024-02-20 18:44:06 -0800

    Revert "Updates"
    This reverts commit 27375d1d8db6df693b5e99f85a99a256ffaefdd4.

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  },
+]);
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);

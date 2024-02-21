9bdaf1acc093822f23255f38ddd51b0ce7776fd8
mckoyd <dominick.mckoy@gmail.com>
2024-02-20 18:32:45 -0800

    updates router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import MenuPage from "./pages/MenuPage";
 import "./App.css";
 import HowToPlayPage from "./pages/HowToPlayPage";
@@ -7,12 +7,10 @@ import HowToPlayPage from "./pages/HowToPlayPage";
 const App: React.FC = () => {
   return (
     <div className="main">
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<MenuPage />} />
-          <Route path="how-to-play" element={<HowToPlayPage />} />
-        </Routes>
-      </HashRouter>
+      <Routes>
+        <Route path="/" element={<MenuPage />} />
+        <Route path="how-to-play" element={<HowToPlayPage />} />
+      </Routes>
     </div>
   );
 };

32dfb8ddb15a15c35fc0907d57d36e213342aa0c
mckoyd <dominick.mckoy@gmail.com>
2024-02-20 18:50:18 -0800

    adds func to back btn

--- src/pages/HowToPlayPage.tsx
+++ src/pages/HowToPlayPage.tsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { ReactComponent as IconBack } from "../assets/images/icon-back.svg";
 import "./HowToPlayPage.css";
 import { howToPlayInfo } from "./HowToPlayPage.config";
 import HowToPlayInfoCard from "../components/HowToPlayInfoCard";
+import { useNavigate } from "react-router-dom";
 
 const HowToPlayPage: React.FC = () => {
+  const navigate = useNavigate();
+  const handleIconBack = useCallback(() => {
+    navigate("/");
+  }, []);
   return (
     <div className="how-to-play-page">
       <div className="nav-banner">
         <div className="icon-back-container">
-          <IconBack className="icon-back" />
+          <IconBack className="icon-back" onClick={handleIconBack} />
         </div>
 
         <p className="title">How to Play</p>
